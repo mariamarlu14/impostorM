81dee68d803b4355053da63c56e15b3ed7466734
mariamarlu14 <mariamarlu14@gmail.com>
2021-01-20 14:05:06 +0100

    control WS

---
+++ cliente/src/controlWeb.js
@@ -0,0 +1,28 @@
+function ControlWeb($){
+
+	this.mostrarCrearPartida=function(){
+		var cadena='<div id="mostrarCP">';
+		cadena=cadena+'<div class="form-group">';
+		cadena=cadena+'<label for="nick">Nick:</label>';
+		cadena=cadena+'<input type="text" class="form-control" id="nick">';
+		cadena=cadena+'</div>';
+		cadena=cadena+'<div class="form-group">';
+		cadena=cadena+'<label for="num">Número:</label>';
+		cadena=cadena+'<input type="text" class="form-control" id="num">';
+		cadena=cadena+'</div>';
+		cadena=cadena+'<button type="button" id="btnCrear" class="btn btn-primary">Crear Partida</button>';
+		cadena=cadena+'</div>';
+
+		$('#crearPartida').append(cadena);
+
+		$('#btnCrear').on('click',function(){
+			var nick=$('#nick').val();
+			var num=$('#num').val();
+			ws.crearPartida(nick, num);
+
+		});
+	}
+	this.mostrarUnirAPartida=function(){
+
+	}
+}
